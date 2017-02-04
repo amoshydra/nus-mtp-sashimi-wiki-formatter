ddc93d28e5e7addabec35e389b69d0d71524802e
Amos Wong <amoswwj@gmail.com>
2017-02-05 02:34:43 +0800

    Use fs-promise

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 const requirementF = require('./libs/requirementF');
 const requirementNF = require('./libs/requirementNF');
-const fs = require('fs');
+var fsp = require('fs-promise');
 
-fs.writeFile('requirements.md', '');
+// Write requirements
+requirementNF()
+.then((data) => {
+    writeTo(data, '3.1-Non-functional-Requirements.md');
+});
 
-requirementNF
-.then(appendFile)
+// Write requirements
+requirementF()
+.then((data) => {
+    writeTo(data, '3.2-Functional-Requirements.md');
+});
 
-.then(() => requirementF)
-.then(appendFile);
-
-function appendFile(data) {
-    fs.appendFile('requirements.md', data);
+function writeTo(data, filename) {
+    filename = `dist/${filename}`
+    return fsp.writeFile(filename, data);
 }
